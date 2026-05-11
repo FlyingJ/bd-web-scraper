1385c62cd8f870f191a017fe3ded3e0ada2cce58
FlyingJ <jason.m.harrington@gmail.com>
2026-05-10 20:49:55 -0400

    Add stub, tests for get_heading_from_html(), get_first_paragraph_from_html()

--- crawl.py
+++ crawl.py
@@ -1,6 +1,14 @@
 from urllib.parse import urlparse
 
+
 def normalize_url(url):
 	url_obj = urlparse(url)
 	print(url_obj)
 	return url_obj.netloc + url_obj.path.rstrip('/')
+
+
+def get_heading_from_html(html):
+	return None
+
+def get_first_paragraph_from_html(html):
+	return None
--- test_crawl.py
+++ test_crawl.py
@@ -1,6 +1,6 @@
 import unittest
 
-from crawl import normalize_url
+import crawl
 
 normalize_url_test_cases = [
 	("https://www.boot.dev/blog/path", "www.boot.dev/blog/path"),
@@ -8,15 +8,31 @@ normalize_url_test_cases = [
 	("http://www.boot.dev/blog/path", "www.boot.dev/blog/path"),
 	("http://www.boot.dev/blog/path/", "www.boot.dev/blog/path"),
 	("ftp://www.boot.dev/blog/path//", "www.boot.dev/blog/path"),
-	("sftp://www.boot.dev//blog/path", "www.boot.dev/blog/path"),
-	("https://example.com/search?stuff+things", "NNNN"),
+	("https://example.com/search?stuff+things", "example.com/search"),
 ]
 
 class TestCrawl(unittest.TestCase):
 	def test_normalize_url(self):
 		for text, expectation in normalize_url_test_cases: 
-			result = normalize_url(text)
+			result = crawl.normalize_url(text)
 			self.assertEqual(result, expectation)
+
+	def test_get_heading_from_html_basic(self):
+	    input_body = '<html><body><h1>Test Title</h1></body></html>'
+	    actual = crawl.get_heading_from_html(input_body)
+	    expected = "Test Title"
+	    self.assertEqual(actual, expected)
+
+	def test_get_first_paragraph_from_html_main_priority(self):
+	    input_body = '''<html><body>
+	        <p>Outside paragraph.</p>
+	        <main>
+	            <p>Main paragraph.</p>
+	        </main>
+	    </body></html>'''
+	    actual = crawl.get_first_paragraph_from_html(input_body)
+	    expected = "Main paragraph."
+	    self.assertEqual(actual, expected)
 
 if __name__ == "__main__":
 	unittest.main()
