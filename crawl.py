41025345bfb83d1a498a7c96b10d7e8d12a41f22
FlyingJ <jason.m.harrington@gmail.com>
2026-06-02 15:21:47 -0400

    Mute crawl.normalize_url()

--- crawl.py
+++ crawl.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 
 def normalize_url(url):
 	url_obj = urlparse(url)
-	print(url_obj)
+	# print(url_obj)
 	return url_obj.netloc + url_obj.path.rstrip('/')
 
 def get_heading_from_html(html):
